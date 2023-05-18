ec4472dafb76c1456f601e8b54b25aadb0f14f62
Alex <fenix_al@tut.by>
2023-05-18 08:59:34 +0300

    re-fix conection error

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,10 +3,8 @@ import { Navigate } from "react-router-dom";
 
 export default function HomePage() {
   const isAuth = useSelector((state) => {
-    console.log(state.user.isAuth);
     return state.user.isAuth;
   });
-  console.log(isAuth);
 
   if (isAuth) return <h1>HomePage</h1>;
   return <Navigate to="/auth" />;
